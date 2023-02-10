f1aace72efe58656313f63d5ea354ccce2c6d194
Adrian Ramirez <adrianramirez@Adrians-MacBook-Air.local>
2023-02-09 21:04:59 -0800

    first commit

---
+++ src/test/java/com/aramirez/jenkinsdocker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aramirez.jenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
